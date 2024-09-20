f11f7eebf3c3d510b67327a25bfad5388ef7f2de
Tomas Turek <tturek@redhat.com>
2024-09-20 10:52:57 +0200

    test: cover if image is enlisted in registry.redhat.io (#6)
    Cover if image is enlisted in registry.redhat.io

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # structural-tests
 Securesign project structural and acceptance tests.
+
+
+## Repository list
+Pull list of repositories from Pyxis API
+
+```bash
+curl --negotiate -u : -b .cookiejar.txt -c .cookiejar.txt 'https://pyxis.engineering.redhat.com/v1/product-listings/id/6604180e80e2fa3e4947d1d5/repositories?filter=release_categories%3Din%3D%28%22Generally%20Available%22%29&include=data.repository,data._id,data.published' | jq > testdata/repositories.json
+```
--- test/support/test_constants.go
+++ test/support/test_constants.go
@@ -2,6 +2,7 @@ package support
 
 const (
 	EnvReleasesSnapshotFile = "SNAPSHOT"
+	EnvRepositoriesFile     = "REPOSITORIES"
 	EnvTestGithubToken      = "TEST_GITHUB_TOKEN" // #nosec G101
 
 	OperatorImageKey       = "rhtas-operator-image"
@@ -12,23 +13,34 @@ const (
 	OperatorTasImageDefinitionRegexp   = `^registry.redhat.io/rhtas/[\w/-]+@sha256:\w{64}$`
 	OtherOperatorImageDefinitionRegexp = `^(registry.redhat.io|registry.access.redhat.com)`
 	SnapshotImageDefinitionRegexp      = `^[\.\w/-]+@sha256:\w{64}$`
+
+	DefaultRepositoriesFile = "testdata/repositories.json"
 )
 
 func MandatoryTasOperatorImageKeys() []string {
 	return []string{
-		"tuf-image",
 		"trillian-log-server-image",
 		"trillian-log-signer-image",
 		"trillian-db-image",
+
+		"fulcio-server-image",
+
 		"rekor-redis-image",
 		"rekor-search-ui-image",
 		"rekor-server-image",
-		"fulcio-server-image",
+		"backfill-redis-image",
+
+		"tuf-image",
+
+		"ctlog-image",
+
 		"client-server-cg-image",
 		"client-server-re-image",
-		"ctlog-image",
-		"backfill-redis-image",
+		"client-server-f-image",
+
 		"segment-backup-job-image",
+
+		"timestamp-authority-image",
 	}
 }
 
