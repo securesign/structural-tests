bca18c8cb75d6217023febf779681870aec7b06c
Petr Pinkas <ppinkas@redhat.com>
2024-10-02 13:04:27 +0200

    Merge with the main branch

--- test/support/test_constants.go
+++ test/support/test_constants.go
@@ -2,6 +2,7 @@ package support
 
 const (
 	EnvReleasesSnapshotFile = "SNAPSHOT"
+	EnvAnsibleImagesFile    = "ANSIBLE"
 	EnvRepositoriesFile     = "REPOSITORIES"
 	EnvTestGithubToken      = "TEST_GITHUB_TOKEN" // #nosec G101
 
@@ -46,5 +47,39 @@ func OtherOperatorImageKeys() []string {
 	return []string{
 		"trillian-netcat-image",
 		"http-server-image",
+	}
+}
+
+func AnsibleImageKeys() []string {
+	return []string{
+		"tas_single_node_fulcio_server_image",
+		"tas_single_node_trillian_log_server_image",
+		"tas_single_node_trillian_log_signer_image",
+		"tas_single_node_rekor_server_image",
+		"tas_single_node_ctlog_image",
+		"tas_single_node_rekor_redis_image",
+		"tas_single_node_trillian_db_image",
+		"tas_single_node_tuf_image",
+		"tas_single_node_timestamp_authority_image",
+		"tas_single_node_rekor_search_ui_image",
+		"tas_single_node_createtree_image",
+		"tas_single_node_client_server_image",
+	}
+}
+
+func OtherAnsibleImageKeys() []string {
+	return []string{
+		"tas_single_node_trillian_netcat_image",
+		"tas_single_node_nginx_image",
+	}
+}
+
+type OSArchMatrix map[string][]string
+
+func GetOSArchMatrix() OSArchMatrix {
+	return map[string][]string{
+		"linux":   {"amd64", "arm64", "ppc64le", "s390x"},
+		"darwin":  {"amd64", "arm64"},
+		"windows": {"amd64"},
 	}
 }
